24a9137307eb293aa9763ef26a6bf1b9a6ad7cad
Dave Guymon <davidwguymon@gmail.com>
2026-08-13 19:29:25 -0600

    Rubocop cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   root "short_links#new"
   get "links", to: "short_links#new", as: :links_page
-  resources :links, only: [:create], controller: "short_links"
+  resources :links, only: [ :create ], controller: "short_links"
   get "/:alias", to: "short_links#show", as: :short_link
 end
